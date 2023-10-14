2a6456c13e5c1f595612afd354f28001c4f307ee
Jonathan Aranguri <arangurim@gmail.com>
2023-10-14 12:36:30 -0300

    feat: Remote config habilitando y deshabilitando la pantalla de About

--- app/src/main/java/com/example/username/myapplication/MainActivity.java
+++ app/src/main/java/com/example/username/myapplication/MainActivity.java
@@ -46,33 +46,30 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String usuario = etUsuario.getText().toString();
-                String password = etPassword.getText().toString();
-                if (usuario.isEmpty() || password.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Completar datos", Toast.LENGTH_SHORT).show();
-                } else {
-                    final Bundle userParams = new Bundle();
-                    userParams.putString("username", usuario);
+        btnIniciarSesion.setOnClickListener(v -> {
+            String usuario = etUsuario.getText().toString();
+            String password = etPassword.getText().toString();
+            if (usuario.isEmpty() || password.isEmpty()) {
+                Toast.makeText(MainActivity.this, "Completar datos", Toast.LENGTH_SHORT).show();
+            } else {
+                final Bundle userParams = new Bundle();
+                userParams.putString("username", usuario);
 
-                    FirebaseAnalytics.getInstance(MainActivity.this).logEvent(
-                            "USER_SIGN_IN", userParams
-                    );
-                    guardarSharedPref(usuario);
+                FirebaseAnalytics.getInstance(MainActivity.this).logEvent(
+                        "USER_SIGN_IN", userParams
+                );
+                guardarSharedPref(usuario);
 
-                    final Trace homeIntentTrace = FirebasePerformance.getInstance().newTrace("HOME_ACTIVITY_INTENT");
+                final Trace homeIntentTrace = FirebasePerformance.getInstance().newTrace("HOME_ACTIVITY_INTENT");
 
-                    homeIntentTrace.start();
-                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                    intent.putExtra("USUARIO", usuario);
-                    intent.putExtra("PASSWORD", password);
-                    homeIntentTrace.stop();
+                homeIntentTrace.start();
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                intent.putExtra("USUARIO", usuario);
+                intent.putExtra("PASSWORD", password);
+                homeIntentTrace.stop();
 
-                    startActivity(intent);
-                    finish();
-                }
+                startActivity(intent);
+                finish();
             }
         });
 
@@ -83,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
         Snackbar.make(container, "En progreso", Snackbar.LENGTH_LONG).show();
     }
 
-    @AddTrace(name = "SAVE_NAME" )
+    @AddTrace(name = "SAVE_NAME")
     private void guardarSharedPref(String usuario) {
         SharedPreferences.Editor editor = pref.edit();
         editor.putString(USUARIO, usuario);
